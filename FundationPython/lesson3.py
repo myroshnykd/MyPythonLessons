3879e188c893e85a0b9f0c0984ecfae19d8ac7d5
myroshnyd <myroshnyk.denys@gmail.com>
2022-05-14 09:57:25 -0700

    Mathematical operations - theory - practic

---
+++ FundationPython/lesson3.py
@@ -0,0 +1,51 @@
+# +
+# -
+# *
+# /
+# // full divide 4/2=2 полное диление
+# ** степень
+# pow() степень
+# % остаток от деления, leftover from dividing
+
+
+# print(6 + 2)
+# print(6 - 2)
+# print(6 * 2)
+# print(6 / 2)
+# print(7 / 2)
+# print(7 // 2)
+# print(6 ** 2)
+# print(pow(6, 2))
+# print(7 % 2)
+# print(6 % 2)
+# print(8 % 3)
+# print(9 % 4)
+# print(45 % 7)
+
+# +=
+# -=
+# *=
+# /=
+# //=
+# **=
+# %=
+
+
+# number = 3
+# print(number)
+# number += 5
+# print(number)
+# number -= 3
+# print(number)
+# number *= 5
+# print(number)
+# number /= 5
+# print(number)
+# number //= 1.5
+# print(number)
+# number **= 1000
+# print(number)
+# number %= 0.7
+# print(number)
+
+
--- Practice/practic1.py
+++ Practice/practic1.py
@@ -16,3 +16,30 @@
 # background_check = input("do you have anything on the record?")
 #
 # print("Hello, please fill out all the required information bellow", name, "what's your DOB" , age, "have you used any other names", question,if(True), if_yes, if(False), "whats your physical address?" address1, "whats your mailing address?", address2, "whats your ocupation status?", ocupation)
+
+
+# hours = int(input("enter hours of sleep: "))
+# minutes = int(input("enter minutes of sleep: "))
+
+# print((hours * 60) + minutes)
+
+
+# minutes = int(input("enter minutes of sleep: "))
+
+# print(int(minutes / 60))
+# print(minutes % 60)
+
+
+minutesX = int(input("enter minutes you need in order to get quality sleep: "))
+minutesM = int(input("enter at what minutes you go yo sleep: "))
+hoursH = int(input("enter at what hours you go to sleep: "))
+
+a = (hoursH * 60) + minutesM + minutesX
+
+print(int(a / 60))
+print(int(a % 60))
+
+
+
+
+
