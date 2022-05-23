c8128fd5817da9e7ee5175855ba2eedcf90c6b61
myroshnyd <myroshnyk.denys@gmail.com>
2022-05-23 09:31:55 -0700

    Logical operation #2 - theory

--- FundationPython/lesson3.py
+++ FundationPython/lesson3.py
@@ -49,3 +49,28 @@
 # print(number)
 
 
+# Logical operator
+# and
+# not
+# or
+
+# and - comes true if both sides are correct
+# or - comes true if one of the sides is correct
+# not - comes true if none of the correct
+
+# age = int(input("Please enter your age: "))
+# ocupation = bool(input("whats your ocupation: "))
+
+# print(age > 18 and ocupation == True)
+
+# age = int(input("Whats your age: "))
+# salary = int(input("Whats your salary: "))
+
+# print(age > 18 or salary > 3000)
+
+# question = input("Please enter your message: ")
+# word = input("Please enter word that you looking for: ")
+
+# print(word not in question)
+
+
